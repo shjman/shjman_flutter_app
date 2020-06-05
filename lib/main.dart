9909ef378732a6735e4f25ba29e78a48a62176c1
shjman <shjman@outlook.com>
2020-06-05 18:11:59 +0300

    fully rework to app with fireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:english_words/english_words.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,109 +7,84 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter1111111',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: RandomWords(),
+      title: 'Baby Names',
+      home: MyHomePage(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  RandomWordsState createState() => RandomWordsState();
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = List<WordPair>();
-  final Set<WordPair> _saved = Set<WordPair>();
-  final TextStyle _biggerFont = const TextStyle(fontSize: 30.0);
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator22222222'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.arrow_forward),
-            onPressed: _pushSaved,
-          ),
-        ],
-      ),
-      body: _buildSuggestions(),
+      appBar: AppBar(title: Text('Baby Name Votes')),
+      body: _buildBody(context),
     );
   }
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> _tiles = _saved.map((WordPair pair) {
-            return ListTile(
-              title: Text(
-                pair.asPascalCase,
-                style: _biggerFont,
-              ),
-            );
-          });
-          final List<Widget> _divided = ListTile.divideTiles(
-            context: context,
-            tiles: _tiles,
-            color: Colors.green,
-          ).toList();
-          return Scaffold(
-            appBar: AppBar(actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.access_alarm),
-                onPressed: () {
-                  print('_onAlarmPressed' + ' DateTime.now ${DateTime.now()} ');
-                },
-              )
-            ], title: Text('Saved Suggestions')),
-            body: ListView(children: _divided),
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        textDirection: TextDirection.rtl,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('babyNames').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildList(context, snapshot.data.documents);
       },
     );
   }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> documentSnapshots) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 10.0),
+      children: documentSnapshots.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record._name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.green),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: ListTile(
+          title: Text(record._name),
+          trailing: Text(record._votes.toString()),
+//          onTap: () => record.reference.updateData({"votes": FieldValue.increment(1)}),
+          onTap: () => Firestore.instance.runTransaction((transaction) async {
+            final freshSnapshot = await transaction.get(record.reference);
+            final fresh = Record.fromSnapshot(freshSnapshot);
+            await transaction.update(record.reference, {'votes': fresh._votes + 1});
+          }),
+        ),
+      ),
+    );
+  }
+}
+
+class Record {
+  final String _name;
+  final int _votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        _name = map['name'],
+        _votes = map['votes'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot) : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$_name:$_votes>";
 }
